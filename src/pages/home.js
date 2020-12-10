099ab845124f67aa50a188ea4d82659a911f9722
Mcbuzzerr <mcbuzzer@gmail.com>
2020-12-10 14:03:17 -0700

    Fixed background layering - commented out text FIX

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,41 +1,52 @@
 import { Link as NextLink } from 'next/link'
-import { Button, ButtonGroup, Link, Box, Flex, Text } from "@chakra-ui/react"
+import { Button, ButtonGroup, Link, Box, Flex, Text, Heading } from "@chakra-ui/react"
+
+// read 3 books for what you are trying to learn
+// First book makes no sense. third book starts making sense, 
+//third book makes sense because you've learned the same thing from 3 different perspectives
+
+
 
 const Home = () => (<>
-    <Flex>
         {/* Left half */}
-        <Box w="50vw" h="100vh" backgroundColor="mycolors.4">
-            <Box>    
-                <Text>Alex Bates</Text>
-                <h2>Web Dev</h2>
-                <Link as={NextLink} href="/about" borderColor="gray.300" borderWidth="5px" bgColor="gray.300" borderRadius="2px">
-                    About
-                </Link>
-            </Box>
-        </Box>
+        <Box w="50vw" h="100vh" backgroundColor="mycolors.4" position="fixed" left="o" zIndex="-1"></Box>
         {/* MID */}
-        <Box w="23.5vw" h="100vh" zIndex="2" position="fixed" top="0" left="0">
+        <Box w="23.5vw" h="100vh" position="fixed" top="0" left="0">
             <svg width="100vw" height="100vh">
                 <Box as="circle" cx="50vw" cy="25vh" r="25vh" fill="mycolors.4" />
                 <Box as="circle" cx="50vw" cy="75vh" r="25vh" fill="mycolors.5" />
             </svg>
         </Box>
         {/* Right half */}
-        <Box w="50vw" h="100vh" backgroundColor="mycolors.5">
-            {/* Link to Examples */}
-            <Box>
-                <Link as={NextLink} to="/">
-                    <Box mr="3px" mb="3px"></Box>
-                </Link>
+        <Box w="50vw" h="100vh" backgroundColor="mycolors.5" position="fixed" left="50vw" zIndex="-1"></Box>
+        {/*<Box>    
+                <Heading size="4xl" color="mycolors.5">Alex Bates</Heading>
+                <Heading size="3xl" color="mycolors.5">Web Dev</Heading>
             </Box>
-            {/* Link to Contact */}
-            <Box>
-                <Link as={NextLink} to="/">
-                    <Box mr="3px" mb="3px"></Box>
-                </Link>
-            </Box>
-        </Box>
-    </Flex>
+            <Link 
+                as={NextLink} 
+                href="/about"
+                borderColor="mycolors.5" 
+                borderWidth="5px" 
+                bgColor="mycolors.5" 
+                borderRadius="2px" 
+                color="mycolors.4"
+                zIndex="1"
+            >
+                About Me
+            </Link>
+// Link to Examples
+<Box>
+<Link as={NextLink} to="/">
+    <Box mr="3px" mb="3px"></Box>
+</Link>
+</Box>
+// Link to Contact
+<Box>
+<Link as={NextLink} to="/">
+    <Box mr="3px" mb="3px"></Box>
+</Link>
+</Box>*/}
 </>
 )
 
